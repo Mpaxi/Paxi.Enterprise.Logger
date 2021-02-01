ea00789f14c4ecfe05e8c2088ed0bb40e7ab5393
Murilo Paxi <murilo.paxi@gmail.com>
2021-02-01 11:49:23 -0300

    Initial Commit

---
+++ Paxi.Enterprise.Logger.Sample/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Paxi.Enterprise.Logger.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paxi.Enterprise.Logger.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGlobalExceptionHandlerMiddleware();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Paxi.Enterprise.Logger.Sample", Version = "v1" });
+            });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseGlobalExceptionHandlerMiddleware();
+
+            if (env.IsDevelopment())
+            {
+                //app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Paxi.Enterprise.Logger.Sample v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            
+        }
+    }
+}
---
+++ Paxi.Enterprise.Logger/Extension/GlobalExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Paxi.Enterprise.Logger.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paxi.Enterprise.Logger.Extension
+{
+
+    public static class GlobalExceptionHandlerMiddlewareExtensions
+    {
+        public static IServiceCollection AddGlobalExceptionHandlerMiddleware(this IServiceCollection services)
+        {
+            services.AddTransient<GlobalExceptionHandlerMiddleware>();
+
+            return services;
+        }
+
+        public static void UseGlobalExceptionHandlerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+        }
+    }
+}
